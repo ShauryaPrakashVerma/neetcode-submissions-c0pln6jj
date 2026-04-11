845611e5924220a4ab5c289441a5d0b4cc1af9db
Shaurya Prakash Verma <shauryaverma22@gmail.com>
2026-04-12 00:09:38 +0530

    Add: majority-element - submission-1

---
+++ Data Structures & Algorithms/majority-element/submission-1.py
@@ -0,0 +1,15 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        '''
+        hash_dict = {}
+        for i in nums:
+            hash_dict[i] = hash_dict.get(i,0) + 1
+
+        for key, value in hash_dict.items():
+            if value > (len(nums)//2):
+                return key
+        '''
+
+        # nlogn complexity
+        nums.sort()
+        return nums[len(nums) // 2]
