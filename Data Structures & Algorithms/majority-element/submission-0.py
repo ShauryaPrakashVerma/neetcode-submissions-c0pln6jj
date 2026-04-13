66202ca85d16cb173cfdb92090bd61b11ef4c856
Shaurya Prakash Verma <shauryaverma22@gmail.com>
2026-04-13 22:24:23 +0530

    Delete submission-0.py

--- Data Structures & Algorithms/majority-element/submission-0.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def majorityElement(self, nums: List[int]) -> int:
-        hash_dict = {}
-        for i in nums:
-            hash_dict[i] = hash_dict.get(i,0) + 1
-
-        for key, value in hash_dict.items():
-            if value > (len(nums)//2):
-                return key
